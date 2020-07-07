93f57000e1bafa8868e5f4fcd2c604e7e218c1ac
Jackson Reynolds <jwreynold@gmail.com>
2020-07-07 17:02:30 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,9 +4,10 @@ def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: "
+    line_announcement = "The line is currently:"
     katz_deli.each_with_index do |patron, index|
-      puts "#{index+1}. #{patron}"
+      line_announcement += " #{index+1}. #{patron}"
     end #each
+    puts line_announcement
   end #if
 end #line
