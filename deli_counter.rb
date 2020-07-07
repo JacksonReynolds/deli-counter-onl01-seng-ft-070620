057038ee6e303098785006611cc5359cbbda95a6
Jackson Reynolds <jwreynold@gmail.com>
2020-07-07 16:58:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,10 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0
     puts "The line is currently empty."
+  else
+    puts "The line is currently: "
+    katz_deli.each_with_index do |patron, index|
+      puts "#{index+1}. #{patron}"
+    end #each
   end #if
 end #line
